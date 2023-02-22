fab77c20192eeb3ad6fa2f3eada1c9830c47053a
yogesh.rao <yogesh.rao@bigohtech.com>
2023-02-22 17:12:29 +0530

    Initial Commit

---
+++ Meat Me Sample Application/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Meat Me Sample Application
+//
+//  Created by Yogesh Rao on 22/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Meat Me Sample ApplicationUITests/Meat_Me_Sample_ApplicationUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Meat_Me_Sample_ApplicationUITestsLaunchTests.swift
+//  Meat Me Sample ApplicationUITests
+//
+//  Created by Yogesh Rao on 22/02/23.
+//
+
+import XCTest
+
+final class Meat_Me_Sample_ApplicationUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
